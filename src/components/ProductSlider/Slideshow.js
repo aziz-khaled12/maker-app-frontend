a9dea8484a5bb67c084c3a6b66ea9842df123073
aziz <khaledaziz@yahoo.com>
2024-05-12 16:22:25 +0100

    last changes hh

--- src/components/ProductSlider/Slideshow.js
+++ src/components/ProductSlider/Slideshow.js
@@ -5,11 +5,8 @@ import 'react-slideshow-image/dist/styles.css'
 import './ProductSlider.css'
 const spanStyle = {
   padding: '20px',
-  background: '#efefef',
   color: '#000000'
 }
-
-const array = [1, 2, 3, 4]
 
 const divStyle = {
   display: 'flex',
@@ -19,16 +16,29 @@ const divStyle = {
   height: '400px',
   width: '100%',
 }
-
+const slideImages = [
+  {
+    url: 'https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+    caption: 'Slide 1'
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80',
+    caption: 'Slide 2'
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+    caption: 'Slide 3'
+  },
+];
 
 const Slideshow = () => {
     return (
       <div className="slide-container">
         <Slide>
-         {array.map((slideImage, index)=> (
+         {slideImages.map((slideImage, index)=> (
             <div key={index}>
-              <div style={divStyle}>
-                <span style={spanStyle}>{index}</span>
+              <div style={{ ...divStyle, 'backgroundImage': `url(${slideImage.url})` }}>
+                <span style={spanStyle}>{slideImage.caption}</span>
               </div>
             </div>
           ))} 
